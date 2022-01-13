c110243d02ca5a3567d38ebccb17b8c820f3e338
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-01-13 18:50:23 +0000

    stroke 21 - Manual

--- csharp/Hole5/TaxRate.cs
+++ csharp/Hole5/TaxRate.cs
@@ -4,13 +4,17 @@ namespace Hole5 {
     public class TaxRate {
         private readonly double percent;
 
+        public TaxRate(int percent) {
+            this.percent = percent;
+        }
+
         public Money Apply(Money first, Money total) {
             Double amount = total.value * (percent / 100d);
             return Money.Create(Convert.ToInt32(amount), first.currency);
         }
 
-        public static object Of(int v) {
-            throw new NotImplementedException();
+        public static TaxRate Of(int percent) {
+            return new TaxRate(percent);
         }
     }
 }
