2f998ccf3a49dcc8f8b20eebe023a3c8aa92a281
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-01-21 17:09:32 +0000

    automatic

--- csharp/Hole6/TakeHomeCalculator.cs
+++ csharp/Hole6/TakeHomeCalculator.cs
@@ -16,12 +16,7 @@ namespace Hole6
         {
             List<Money> monies = rest.ToList();
 
-            Money total = first;
-
-            foreach (Money next in monies)
-            {
-                total = total.Plus(next);
-            }
+            Money total = monies.Aggregate(first, (current, next) => current.Plus(next));
 
             Money tax = taxRate.Apply(total);
             return total.Minus(tax);
