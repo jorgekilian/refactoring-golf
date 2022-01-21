54413dbe601e669300c0a4d50f25cc4d9c53a0d4
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-01-21 16:57:32 +0000

    automatic

---
+++ csharp/Hole5/TaxRate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hole5 {
+    public class TaxRate {
+        private TakeHomeCalculator percent;
+        public TaxRate(int percent) 
+        {
+            this.percent = percent;
+        }
+
+        public Money Apply(Money total) 
+        {
+            Double amount = total.value * (percent.percent / 100d);
+            return Money.Create(Convert.ToInt32(amount), total.currency);
+        }
+    }
+}
